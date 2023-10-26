beec7ee4feb78e7cfcd6a714b9b2fb860c0ec58d
Dragos Daian <daian.dragos@yahoo.com>
2023-10-26 15:43:43 +0200

    upd

---
+++ scripts/crossplatform-sed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# From https://gist.github.com/maxpoletaev/4ed25183427a2cd7e57a
+
+case "$OSTYPE" in
+    darwin*)  PLATFORM="OSX" ;;
+    linux*)   PLATFORM="LINUX" ;;
+    bsd*)     PLATFORM="BSD" ;;
+    *)        PLATFORM="UNKNOWN" ;;
+esac
+
+echo $PLATFORM
+
+if [[ "$PLATFORM" == "OSX" || "$PLATFORM" == "BSD" ]]; then
+    sed -i "" "$1" "$2"
+elif [ "$PLATFORM" == "LINUX" ]; then
+    sed -i "$1" "$2"
+fi
+
+echo "Done"
