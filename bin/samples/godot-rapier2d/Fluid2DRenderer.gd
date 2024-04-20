aad8f3c44d41c09f35e2a9b6ed40e72447d67854
Dragos Daian <daian.dragos@yahoo.com>
2024-04-20 19:06:02 +0200

    update2

--- bin/samples/godot-rapier2d/Fluid2DRenderer.gd
+++ bin/samples/godot-rapier2d/Fluid2DRenderer.gd
@@ -12,11 +12,8 @@ func _process(_delta):
 	var index = 0
 	multimesh.instance_count = fluid.points.size()
 	var points = fluid.points
-	var create_times = fluid.get_create_times()
-	var current_time = Time.get_ticks_msec()
 	for i in points.size():
 		var point = points[i]
-		var created_at = create_times[i]
 		var new_transform: Transform2D = Transform2D(0, Vector2(5, 5), 0, point)
 		multimesh.set_instance_transform_2d(index, new_transform)
 		multimesh.set_instance_color(index, color)
