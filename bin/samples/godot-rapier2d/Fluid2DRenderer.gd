4314293c7306c9234af5adf22cda9febc24e676c
Anton <noten.b@hotmail.com>
2024-04-20 19:12:06 +0200

    Use viewport for fluid rendering instead of mask color 💦 (#78)
    Marking as draft at the moment since the camera is broken.
    
    ---------
    
    Co-authored-by: Dragos Daian <daian.dragos@yahoo.com>

--- bin/samples/godot-rapier2d/Fluid2DRenderer.gd
+++ bin/samples/godot-rapier2d/Fluid2DRenderer.gd
@@ -1,23 +1,20 @@
 class_name Fluid2DRenderer
 extends MultiMeshInstance2D
 
-@onready var fluid :Fluid2D = get_parent()
+@export var fluid: Fluid2D
 @export var color: Color
 
 func _ready():
 	multimesh = multimesh.duplicate(true)
 
 func _process(_delta):
+	global_position = fluid.global_position
 	var index = 0
 	multimesh.instance_count = fluid.points.size()
 	var points = fluid.points
-	var create_times = fluid.get_create_times()
-	var current_time = Time.get_ticks_msec()
 	for i in points.size():
 		var point = points[i]
-		var created_at = create_times[i]
-		var new_transform: Transform2D = Transform2D()
-		new_transform.origin = point - position
+		var new_transform: Transform2D = Transform2D(0, Vector2(5, 5), 0, point)
 		multimesh.set_instance_transform_2d(index, new_transform)
 		multimesh.set_instance_color(index, color)
 		index += 1
