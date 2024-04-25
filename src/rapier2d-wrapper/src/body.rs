bd0ed62a956cf5499675ede6f435ecb6ed372625
Dragos Daian <daian.dragos@yahoo.com>
2024-04-25 19:55:53 +0200

    Update body.rs

--- src/rapier2d-wrapper/src/body.rs
+++ src/rapier2d-wrapper/src/body.rs
@@ -212,7 +212,7 @@ pub extern "C" fn body_set_can_sleep(world_handle : Handle, body_handle : Handle
     
     if !can_sleep && body.activation().angular_threshold != -1.0 {
 		let activation = body.activation_mut();
-        activation.normalized_angular_threshold = -1.0;
+        activation.angular_threshold = -1.0;
         activation.normalized_linear_threshold = -1.0;
     }
 
