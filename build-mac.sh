89b18681f89ecb13454a6123a1787ecbdaaf3098
Dragos Daian <daian.dragos@yahoo.com>
2024-04-26 10:22:50 +0200

    upd

---
+++ build-mac.sh
@@ -0,0 +1,2 @@
+cargo build
+mv target/debug/libgodot_rapier.dylib bin/addons/godot-rapier2d/bin/libphysics_server_rapier2d.macos.template_debug.framework/libphysics_server_rapier2d.macos.template_debug.dylib
