5c3868990eeab95cf4cbd2187870a35d5a58735d
Dragos Daian <daian.dragos@yahoo.com>
2024-06-08 18:10:09 +0200

    update

--- scripts/build-mac-dev.sh
+++ scripts/build-mac-dev.sh
@@ -1,5 +1,6 @@
 cargo fmt
 cargo clippy --fix --allow-dirty
 cargo build --features="single_dim2,parallel,simd-stable" --no-default-features
-cp target/debug/libgodot_rapier.dylib bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+#cp target/debug/libgodot_rapier.dylib bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+cp target/debug/libgodot_rapier.dylib bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.dylib
 
