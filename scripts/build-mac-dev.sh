7cef7ac8457027c4ad8fbe1795ac2b7ac530108b
Dragos Daian <daian.dragos@yahoo.com>
2024-06-08 17:35:21 +0200

    use parallel and simd-stable from feature

--- scripts/build-mac-dev.sh
+++ scripts/build-mac-dev.sh
@@ -1,5 +1,5 @@
 cargo fmt
 cargo clippy --fix --allow-dirty
-cargo build --features="single_dim2,rapier2d/parallel,rapier2d/simd-stable" --no-default-features
+cargo build --features="single_dim2,parallel,simd-stable" --no-default-features
 cp target/debug/libgodot_rapier.dylib bin/addons/godot-rapier2d/bin/libphysics_server_rapier2d.macos.template_debug.framework/libphysics_server_rapier2d.macos.template_debug.dylib
 
