e2db85aa8b4e16d82242cd9c87b5e839d1b15797
Dragos Daian <daian.dragos@yahoo.com>
2024-06-17 10:23:01 +0200

    upd

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,8 +1,5 @@
 cargo clippy --fix --allow-dirty
-cargo +nightly build -Z build-std=panic_abort,std --features="simd-stable,experimental-wasm" --target wasm32-unknown-emscripten
 cargo +nightly build -Z build-std=panic_abort,std --release --features="simd-stable,experimental-wasm" --target wasm32-unknown-emscripten
-cp target/wasm32-unknown-emscripten/debug/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
-cp target/wasm32-unknown-emscripten/debug/godot_rapier.wasm Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
 
 cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
-cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
+cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
