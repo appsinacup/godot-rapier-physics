5630bcd931d88733486e2dff1ee93c6a2da659c0
Dragos Daian <daian.dragos@yahoo.com>
2024-06-17 12:35:45 +0200

    add install ndk step

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,5 +1,5 @@
 cargo clippy --fix --allow-dirty
-cargo +nightly build -Z build-std=panic_abort,std --release --features="simd-stable,experimental-wasm" --target wasm32-unknown-emscripten
+cargo +nightly build -Z build-std=panic_abort,std --release --features="simd-stable,experimental-wasm,wasm-bindgen" --target wasm32-unknown-emscripten --no-default-features
 
 cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
 cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
