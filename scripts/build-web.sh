c53bd51fce1f6b360020ba9076eb362964a61f3d
Dragos Daian <daian.dragos@yahoo.com>
2024-06-24 11:39:56 +0200

    update

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,5 +1,6 @@
+cargo fmt -- --config-path rustfmt.toml
 cargo clippy --fix --allow-dirty
-cargo +nightly build -Z build-std=panic_abort,std --release --features="simd-stable,experimental-wasm,wasm-bindgen" --target wasm32-unknown-emscripten --no-default-features
+cargo +nightly build -Z build-std=panic_abort,std --release --features="single-dim2,experimental-wasm,wasm-bindgen" --target wasm32-unknown-emscripten --no-default-features
 
 cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
 cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
