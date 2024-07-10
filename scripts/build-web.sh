df0caedac6bf3de0d1093fbe2d046510e0b38cb8
Dragos Daian <daian.dragos@yahoo.com>
2024-07-10 22:35:48 +0200

    upd

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -4,8 +4,8 @@ cargo clippy --fix --allow-dirty
 #cargo build -Z build-std=panic_abort,std --features="single-dim2,experimental-wasm" --target wasm32-unknown-emscripten --no-default-features --release
 cargo build -Zbuild-std --features="single-dim2,experimental-wasm,wasm-bindgen" --target wasm32-unknown-emscripten --no-default-features
 
-#cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
-#cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
+#cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm
+#cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm
 
-cp target/wasm32-unknown-emscripten/debug/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
-cp target/wasm32-unknown-emscripten/debug/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm32.wasm
+cp target/wasm32-unknown-emscripten/debug/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm
+cp target/wasm32-unknown-emscripten/debug/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm
