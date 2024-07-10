79ae94c975bef08d40b26b353d6062f7572132d4
Dragos Daian <daian.dragos@yahoo.com>
2024-07-10 22:52:20 +0200

    upd

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -1,8 +1,8 @@
 cargo fmt -- --config-path rustfmt.toml
 cargo clippy --fix --allow-dirty
-#set EMCC_CFLAGS=-s ERROR_ON_UNDEFINED_SYMBOLS=0 --no-entry -gsource-map
-#cargo build -Z build-std=panic_abort,std --features="single-dim2,experimental-wasm" --target wasm32-unknown-emscripten --no-default-features --release
-cargo build -Zbuild-std --features="single-dim2,experimental-wasm,wasm-bindgen" --target wasm32-unknown-emscripten --no-default-features
+set EMCC_CFLAGS=-s ERROR_ON_UNDEFINED_SYMBOLS=0 --no-entry -gsource-map
+#cargo build -Zbuild-std --features="single-dim2,experimental-wasm" --target wasm32-unknown-emscripten --no-default-features --release
+cargo build -Zbuild-std --features="single-dim2,experimental-wasm,serde-serialize" --target wasm32-unknown-emscripten --no-default-features
 
 #cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm
 #cp target/wasm32-unknown-emscripten/release/godot_rapier.wasm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.web.wasm
