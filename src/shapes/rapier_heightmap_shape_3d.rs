4039f8073c3729dd6dbdfb26795eefa0ea0c1022
Dragos Daian <daian.dragos@yahoo.com>
2024-08-03 12:11:40 +0200

    Update rapier_heightmap_shape_3d.rs

--- src/shapes/rapier_heightmap_shape_3d.rs
+++ src/shapes/rapier_heightmap_shape_3d.rs
@@ -14,7 +14,7 @@ use crate::types::*;
 )]
 pub struct RapierHeightMapShape3D {
     // TODO serialize this
-    #[cfg_attr(feature = "serde-serialize", serde(skip), default = "invalid_rid")]
+    #[cfg_attr(feature = "serde-serialize", serde(skip))]
     heights: PackedFloatArray,
     width: i32,
     depth: i32,
