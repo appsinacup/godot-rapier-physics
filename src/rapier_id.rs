7e87e17534e530c84736aa613bba71f05c3d91bf
Dragos Daian <daian.dragos@yahoo.com>
2024-08-31 11:21:30 +0200

    Update rapier_id.rs

--- src/rapier_id.rs
+++ src/rapier_id.rs
@@ -1,10 +1,14 @@
+use std::sync::atomic::AtomicUsize;
+use core::sync::atomic::Ordering;
+
 #[derive(PartialEq, Eq, Hash, Clone, Copy)]
 #[cfg_attr(feature = "serde-serialize", derive(serde::Serialize, serde::Deserialize))]
 pub struct RapierID {
-    pub id: i32,
+    pub id: usize,
 }
 impl RapierID {
     pub fn new() -> Self {
-        Self { id: rand::random() }
+        static COUNTER:AtomicUsize = AtomicUsize::new(1);
+        Self { id: COUNTER.fetch_add(1, Ordering::Relaxed) }
     }
 }
