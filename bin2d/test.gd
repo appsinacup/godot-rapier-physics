b5fc393f31440e5b2ddcc8afc02f99cf5bdba038
Dragos Daian <daian.dragos@yahoo.com>
2024-09-13 18:19:43 +0200

    update 3d tests

--- bin2d/test.gd
+++ bin2d/test.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
--- bin3d/test.gd
+++ bin3d/test.gd
@@ -1,14 +1,36 @@
-extends Node2D
+extends Node
 
-@export var body: RigidBody2D
 
-func _on_timer_timeout():
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Start")
+	test_capsule_shape()
+	test_circle_shape()
+	test_concave_polygon_shape()
+	print("Success")
+	print("Quit")
+	await get_tree().create_timer(1.0).timeout
 	get_tree().quit()
 
-func _ready():
-	var space := get_viewport().find_world_2d().direct_space_state as RapierDirectSpaceState2D
-	var physics_server = PhysicsServer2D
-	#physics_server.body_set_extra_param(body.get_rid(), 0, 20.0)
+func test_capsule_shape():
+	RapierCapsuleShapeTests.test_allows_one_way_collision()
+	RapierCapsuleShapeTests.test_create()
+	RapierCapsuleShapeTests.test_get_data()
+	RapierCapsuleShapeTests.test_get_type()
+	RapierCapsuleShapeTests.test_set_data_from_array()
+	RapierCapsuleShapeTests.test_set_data_from_dictionary()
+	RapierCapsuleShapeTests.test_set_data_from_vector2()
+	
+func test_circle_shape():
+	RapierCircleShapeTests.test_allows_one_way_collision()
+	RapierCircleShapeTests.test_create()
+	RapierCircleShapeTests.test_get_data()
+	RapierCircleShapeTests.test_get_type()
+	RapierCircleShapeTests.test_set_data()
 
-	#var space_json := space.export_json()
-	#print(space_json)
+func test_concave_polygon_shape():
+	RapierConcavePolygonShapeTests.test_allows_one_way_collision()
+	RapierConcavePolygonShapeTests.test_create()
+	RapierConcavePolygonShapeTests.test_get_data()
+	RapierConcavePolygonShapeTests.test_get_type()
+	RapierConcavePolygonShapeTests.test_set_data()
