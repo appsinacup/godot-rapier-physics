fe5e0844fe0fe1f577e8b6a76db74a78e6d7143c
Dragos Daian <daian.dragos@yahoo.com>
2024-09-13 18:31:12 +0200

    Update build-dev-3d.sh

--- scripts/build-dev-3d.sh
+++ scripts/build-dev-3d.sh
@@ -1,13 +1,14 @@
 cargo fmt -- --config-path rustfmt.toml
 cargo clippy --fix --allow-dirty
 cargo build --features="build3d,test" --no-default-features
-rm bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-#rm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-#rm /Users/dragosdaian/Documents/GitHub/Godot-Physics-Tests/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-cp target/debug/libgodot_rapier.dylib bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-#cp target/debug/libgodot_rapier.dylib /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-#cp target/debug/libgodot_rapier.dylib /Users/dragosdaian/Documents/GitHub/Godot-Physics-Tests/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-
-#xattr -rc bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-#xattr -rc /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
-#xattr -rc /Users/dragosdaian/Documents/GitHub/Godot-Physics-Tests/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    echo "Running on macOS"
+    rm bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+    cp target/debug/libgodot_rapier.dylib bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+else
+    # Linux
+    echo "Running on Linux"
+    rm bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.so
+    cp target/debug/libgodot_rapier.so bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.so
+fi
