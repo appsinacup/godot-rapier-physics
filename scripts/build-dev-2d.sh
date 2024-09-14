1f6fb89eea149cff0427f4e6cd3290aaa819c244
Dragos Daian <daian.dragos@yahoo.com>
2024-09-14 11:12:17 +0200

    -f flag

--- scripts/build-dev-2d.sh
+++ scripts/build-dev-2d.sh
@@ -4,11 +4,11 @@ cargo build --features="build2d,test" --no-default-features
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
     echo "Running on macOS"
-    rm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+    rm -f bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
     cp target/debug/libgodot_rapier.dylib bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
 else
     # Linux
     echo "Running on Linux"
-    rm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.linux.so
+    rm -f bin2d/addons/godot-rapier2d/bin/libgodot_rapier.linux.so
     cp target/debug/libgodot_rapier.so bin2d/addons/godot-rapier2d/bin/libgodot_rapier.linux.so
 fi
--- scripts/build-dev-3d.sh
+++ scripts/build-dev-3d.sh
@@ -4,11 +4,11 @@ cargo build --features="build3d,test" --no-default-features
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
     echo "Running on macOS"
-    rm bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+    rm -f bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
     cp target/debug/libgodot_rapier.dylib bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
 else
     # Linux
     echo "Running on Linux"
-    rm bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.so
+    rm -f bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.so
     cp target/debug/libgodot_rapier.so bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.so
 fi
