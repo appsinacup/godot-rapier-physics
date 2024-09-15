b387591936f6e44d68b8a8dfd0b688949a50ddfa
Dragos Daian <daian.dragos@yahoo.com>
2024-09-15 23:26:36 +0200

    update test

--- scripts/build-dev-3d.sh
+++ scripts/build-dev-3d.sh
@@ -1,13 +1,12 @@
 cargo fmt -- --config-path rustfmt.toml
 cargo clippy --fix --allow-dirty
-cargo build --features="build3d,test" --no-default-features
 if [ "${OSTYPE#darwin}" != "$OSTYPE" ]; then
-    # macOS
+    cargo build --features="build3d,test" --no-default-features
     echo "Running on macOS"
     rm -f bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
     cp target/debug/libgodot_rapier.dylib bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
 else
-    # Linux
+    cargo build --features="build3d,test" --no-default-features --target=x86_64-unknown-linux-gnu
     echo "Running on Linux"
-    cp target/debug/libgodot_rapier.so bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.x86_64-unknown-linux-gnu.so
+    cp target/x86_64-unknown-linux-gnu/debug/libgodot_rapier.so bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.x86_64-unknown-linux-gnu.so
 fi
