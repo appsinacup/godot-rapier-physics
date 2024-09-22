6a30265452dc063393ab13d9484243674156dddf
Dragos Daian <daian.dragos@yahoo.com>
2024-09-22 14:31:08 +0200

    Update physics_world.rs

--- src/rapier_wrapper/physics_world.rs
+++ src/rapier_wrapper/physics_world.rs
@@ -408,7 +408,7 @@ impl PhysicsWorld {
                 let joint = self
                     .physics_objects
                     .impulse_joint_set
-                    .get_mut(ImpulseJointHandle(handle.index), true);
+                    .get_mut(ImpulseJointHandle(handle.index));
                 if let Some(joint) = joint {
                     return Some(&mut joint.data);
                 }
