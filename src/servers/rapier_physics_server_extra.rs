485a6f27aef6f1b478ef77d728709a61604e383f
Dragos Daian <daian.dragos@yahoo.com>
2024-09-23 21:59:53 +0200

    Add some functions

--- src/servers/rapier_physics_server_extra.rs
+++ src/servers/rapier_physics_server_extra.rs
@@ -1,6 +1,9 @@
+use std::f32::NAN;
+
 use godot::global::rid_allocate_id;
 use godot::global::rid_from_int64;
 use godot::prelude::*;
+use rapier::na::ComplexField;
 
 use super::rapier_physics_singleton::physics_data;
 use crate::bodies::rapier_collision_object::IRapierCollisionObject;
@@ -382,8 +385,84 @@ impl RapierPhysicsServer {
     }
 
     #[func]
-    fn noop() {}
+    /// Deterministically compute acos.
+    fn acos(x: real) -> real {
+        ComplexField::acos(x)
+    }
 
     #[func]
-    fn noop_binded(&self) {}
+    /// Deterministically compute acosh.
+    fn acosh(x: real) -> real {
+        ComplexField::acosh(x)
+    }
+
+    #[func]
+    /// Deterministically compute asin.
+    fn asin(x: real) -> real {
+        ComplexField::asin(x)
+    }
+
+    #[func]
+    /// Deterministically compute asinh.
+    fn asinh(x: real) -> real {
+        ComplexField::asinh(x)
+    }
+
+    #[func]
+    /// Deterministically compute atan.
+    fn atan(x: real) -> real {
+        ComplexField::atan(x)
+    }
+
+    #[func]
+    /// Deterministically compute atanh.
+    fn atanh(x: real) -> real {
+        ComplexField::atanh(x)
+    }
+
+    #[func]
+    /// Deterministically compute cos.
+    fn cos(x: real) -> real {
+        ComplexField::cos(x)
+    }
+
+    #[func]
+    /// Deterministically compute cosh.
+    fn cosh(x: real) -> real {
+        ComplexField::cosh(x)
+    }
+    #[func]
+    /// Deterministically compute log.
+    fn log(x: real) -> real {
+        ComplexField::ln(x)
+    }
+    #[func]
+    /// Deterministically compute sin.
+    fn sin(x: real) -> real {
+        ComplexField::sin(x)
+    }
+    #[func]
+    /// Deterministically compute sinh.
+    fn sinh(x: real) -> real {
+        ComplexField::sinh(x)
+    }
+    #[func]
+    /// Deterministically compute tan.
+    fn tan(x: real) -> real {
+        ComplexField::tan(x)
+    }
+    #[func]
+    /// Deterministically compute tanh.
+    fn tanh(x: real) -> real {
+        ComplexField::tanh(x)
+    }
+
+    #[func]
+    /// Deterministically compute sqrt.
+    fn sqrt(x: real) -> real {
+        if let Some(result) = ComplexField::try_sqrt(x) {
+            return result;
+        }
+        NAN
+    }
 }
