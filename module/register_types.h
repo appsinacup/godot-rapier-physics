6630ba5606d7157997dea5a027f4b55505ccc6bb
Dragos Daian <daian.dragos@yahoo.com>
2024-11-03 20:00:16 +0100

    fix pattern. Add module

---
+++ module/register_types.h
@@ -0,0 +1,9 @@
+#ifndef MY_MODULE_REGISTER_TYPES_H
+#define MY_MODULE_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+void initialize_my_module_module(ModuleInitializationLevel p_level);
+void uninitialize_my_module_module(ModuleInitializationLevel p_level);
+
+#endif // MY_MODULE_REGISTER_TYPES_H
