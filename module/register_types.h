21a1fa4f50a24c687028955555a55f22ed825531
Dragos Daian <daian.dragos@yahoo.com>
2024-11-04 11:12:52 +0100

    Update register_types.h

--- module/register_types.h
+++ module/register_types.h
@@ -1,9 +1,9 @@
-#ifndef MY_MODULE_REGISTER_TYPES_H
-#define MY_MODULE_REGISTER_TYPES_H
+#ifndef RAPIER_REGISTER_TYPES_H
+#define RAPIER_REGISTER_TYPES_H
 
 #include "modules/register_module_types.h"
 
-void initialize_my_module_module(ModuleInitializationLevel p_level);
-void uninitialize_my_module_module(ModuleInitializationLevel p_level);
+void initialize_rapier_module(ModuleInitializationLevel p_level);
+void uninitialize_rapier_module(ModuleInitializationLevel p_level);
 
-#endif // MY_MODULE_REGISTER_TYPES_H
+#endif // RAPIER_REGISTER_TYPES_H
