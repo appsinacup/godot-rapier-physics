0ae30ab1e68f06a2fe5d5b8dfea222a45a6f0101
Dragos Daian <daian.dragos@yahoo.com>
2025-10-22 14:24:17 +0200

    Remove now unused module folder, as that info will be on a different repo.

--- module3d/register_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef RAPIER_3D_REGISTER_TYPES_H
-#define RAPIER_3D_REGISTER_TYPES_H
-
-#include "modules/register_module_types.h"
-
-void initialize_rapier_3d_module(ModuleInitializationLevel p_level);
-void uninitialize_rapier_3d_module(ModuleInitializationLevel p_level);
-
-#endif // RAPIER_3D_REGISTER_TYPES_H
