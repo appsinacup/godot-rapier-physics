4d56f78fa1f64111de23e47ca69d2a408a68c212
Dragos Daian <daian.dragos@yahoo.com>
2025-11-07 14:38:25 +0100

    update lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,6 @@
 cargo fmt --all -- --check
+cargo clippy --fix --allow-dirty --all-targets --features="build2d" --no-default-features
+cargo clippy --fix --allow-dirty --all-targets --features="build3d" --no-default-features
+cargo clippy --fix --allow-dirty
 gdlint bin2d/addons/godot-rapier2d/**.gd
 gdlint bin3d/addons/godot-rapier3d/**.gd
