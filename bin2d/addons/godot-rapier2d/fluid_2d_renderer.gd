965b55660cc17c8091b43108e8c0837484536ef9
nenado <17idontwant@gmail.com>
2026-04-26 10:16:31 +0300

    Refactor Fluid2DRenderer for improved readability

--- bin2d/addons/godot-rapier2d/fluid_2d_renderer.gd
+++ bin2d/addons/godot-rapier2d/fluid_2d_renderer.gd
@@ -2,30 +2,30 @@
 class_name Fluid2DRenderer
 extends MultiMeshInstance2D
 
-@export var fluid: Fluid2D
+@export var fluid: Fluid2D:
+	set(v): fluid = v; set_process(fluid != null)
 @export var color: Color = Color(0.8, 0.8, 0.8, 0.3)
-@export var mesh_scale: Vector2 = Vector2(5, 5)
+@export_custom(PROPERTY_HINT_LINK, "") var mesh_scale: Vector2 = Vector2(5, 5)
+
+const RADIAL_2D_TEXTURE := preload("uid://cho3shol3rky2")
+const CIRCLE_MESH := preload("uid://dahp28qij58i1")
 
 
 func _ready():
 	if multimesh == null:
 		multimesh = MultiMesh.new()
-		multimesh.mesh = load("res://addons/godot-rapier2d/circle_mesh.tres").duplicate()
+		multimesh.mesh = CIRCLE_MESH.duplicate()
 		multimesh.use_colors = true
 	if texture == null:
-		texture = load("res://addons/godot-rapier2d/Radial2D.svg")
+		texture = RADIAL_2D_TEXTURE
 
 
 func _process(_delta):
 	if fluid == null:
+		set_process(false)
 		return
 	global_transform = fluid.global_transform
-	var index = 0
 	multimesh.instance_count = fluid.points.size()
-	var points = fluid.points
-	for i in points.size():
-		var point = points[i]
-		var new_transform: Transform2D = Transform2D(0, mesh_scale, 0, point)
-		multimesh.set_instance_transform_2d(index, new_transform)
-		multimesh.set_instance_color(index, color)
-		index += 1
+	for i in fluid.points.size():
+		multimesh.set_instance_transform_2d(i, Transform2D(0, mesh_scale, 0, fluid.points[i]))
+		multimesh.set_instance_color(i, color)
