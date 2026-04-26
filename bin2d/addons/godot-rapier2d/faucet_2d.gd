57bf70ec773a2bb9c248b0ab511575e58ef44ff1
nenado <17idontwant@gmail.com>
2026-04-26 10:17:50 +0300

    Refactor Faucet2D

--- bin2d/addons/godot-rapier2d/faucet_2d.gd
+++ bin2d/addons/godot-rapier2d/faucet_2d.gd
@@ -1,27 +1,29 @@
 class_name Faucet2D
 extends Fluid2D
 
-@export var interval := 0.06
-@export var max_particles: int = 1000
-@export var width: int = 4
-@export var height: int = 2
+@export_range(0, 1, 0.01, "or_greater") var interval: float = 0.06
+@export_range(0, 1024, 1, "or_greater") var max_particles: int = 1000
+@export_range(0, 8, 1, "or_greater") var width: int = 4
+@export_range(0, 4, 1, "or_greater") var height: int = 2
 
 var points_new: PackedVector2Array
 var velocities_new: PackedVector2Array
 
+var timer: Timer
 
 func _ready():
+	timer = Timer.new()
+	timer.name = &"Fluid2DTimer"
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer, false, Node.INTERNAL_MODE_DISABLED)
+	#timer.one_shot = true
 	points_new = create_rectangle_points(width, height)
 	velocities_new.resize(points_new.size())
-	var gravity_value = ProjectSettings.get("physics/2d/default_gravity")
-	var gravity_dir = ProjectSettings.get("physics/2d/default_gravity_vector")
-	var dir = global_transform.basis_xform(gravity_dir * gravity_value)
-	velocities_new.fill(dir)
-	get_tree().create_timer(interval).timeout.connect(_on_timer_timeout)
+	velocities_new.fill(global_transform.basis_xform(ProjectSettings.get("physics/2d/default_gravity_vector") * ProjectSettings.get("physics/2d/default_gravity")))
+	timer.start(interval)
 
 
 func _on_timer_timeout():
-	get_tree().create_timer(interval).timeout.connect(_on_timer_timeout)
-	if len(points) > max_particles:
+	if points.size() > max_particles:
 		return
 	add_points_and_velocities(points_new, velocities_new)
