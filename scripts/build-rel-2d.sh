514da961d6ab6644ec273a53e40de2ce2902f2c6
Dragos Daian <daian.dragos@yahoo.com>
2026-06-23 23:22:51 +0200

    remove need for api-custom for non nightly build
    upd readme
    
    add removed by mistake
    
    fix api-custom on ci/cd.

--- scripts/build-rel-2d.sh
+++ scripts/build-rel-2d.sh
@@ -1,7 +1,6 @@
 cargo fmt -- --config-path rustfmt.toml
-cargo clippy --fix --allow-dirty --features="single-dim2,simd-stable,serde-serialize,parallel,experimental-threads,register-docs,api-4-7,api-custom"
-export GDRUST_GODOT_BIN="${GDRUST_GODOT_BIN:-/Applications/Godot.app/Contents/MacOS/Godot}"
-cargo build --release --features="single-dim2,simd-stable,serde-serialize,parallel,experimental-threads,register-docs,api-4-7,api-custom"
+cargo clippy --fix --allow-dirty --features="single-dim2,simd-stable,serde-serialize,parallel,experimental-threads,register-docs,api-4-7"
+cargo build --release --features="single-dim2,simd-stable,serde-serialize,parallel,experimental-threads,register-docs,api-4-7"
 rm bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
 rm /Users/dragosdaian/Documents/Godot-Physics-Tests/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
 cp target/release/libgodot_rapier.dylib bin2d/addons/godot-rapier2d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
