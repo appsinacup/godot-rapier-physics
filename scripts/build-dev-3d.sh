d2763b9767fb3a72d6e4520c22c0f8b1ab086800
Bent Hillerkus <bent@bent.party>
2026-09-09 01:32:42 +0200

    scripts: support windows/cygwin

--- scripts/build-dev-3d.sh
+++ scripts/build-dev-3d.sh
@@ -1,12 +1,17 @@
 cargo fmt -- --config-path rustfmt.toml
-cargo clippy --fix --allow-dirty --features="single-dim3,serde-serialize,test,register-docs,api-4-7"
+FEATURES="single-dim3,serde-serialize,test,register-docs,api-4-7"
+cargo clippy --fix --allow-dirty --features="$FEATURES"
 if [ "${OSTYPE#darwin}" != "$OSTYPE" ]; then
-    cargo build --features="single-dim3,serde-serialize,test,register-docs,api-4-7"
+    cargo build --features="$FEATURES"
     echo "Running on macOS"
     rm -f bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
     cp target/debug/libgodot_rapier.dylib bin3d/addons/godot-rapier3d/bin/libgodot_rapier.macos.framework/libgodot_rapier.macos.dylib
+elif [ "$OSTYPE" = "cygwin" ]; then
+    cargo build --features="$FEATURES" --target=x86_64-pc-windows-msvc
+    echo "Running on Windows"
+    cp target/x86_64-pc-windows-msvc/debug/godot_rapier.dll bin3d/addons/godot-rapier3d/bin/libgodot_rapier.windows.x86_64-pc-windows-msvc.dll
 else
-    cargo build --features="single-dim3,serde-serialize,test,register-docs,api-4-7" --target=x86_64-unknown-linux-gnu
+    cargo build --features="$FEATURES" --target=x86_64-unknown-linux-gnu
     echo "Running on Linux"
     cp target/x86_64-unknown-linux-gnu/debug/libgodot_rapier.so bin3d/addons/godot-rapier3d/bin/libgodot_rapier.linux.x86_64-unknown-linux-gnu.so
 fi
